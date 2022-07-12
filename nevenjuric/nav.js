7926a4c2237c60138db268948bdcc6a3d1d2aba4
LeoJuric <109168277+LeoJuric@users.noreply.github.com>
2022-07-12 21:19:34 +0200

    Add files via upload

---
+++ nevenjuric/nav.js
@@ -0,0 +1,58 @@
+const hamburger = document.querySelector('.hamburger');
+const nav = document.querySelector('.primary-navigation');
+
+nav.style.display = "none";
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active')
+});
+
+document.querySelector('.hamburger').onclick = dropdown
+function dropdown() {
+    if (nav.style.display == 'none') {
+        nav.style.display = 'block'
+    }
+    else {
+        nav.style.display = 'none'
+    }
+}
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 1000,
+    delay: 200,
+})
+
+sr.reveal('#about1')
+sr.reveal('#about2')
+sr.reveal('#ar2h1', {origin: 'left'})
+sr.reveal('#ar2p', {origin: 'left'})
+sr.reveal('#ar2ul', {origin: 'left'})
+sr.reveal('#ar2if', {origin: 'right'})
+sr.reveal('#h1h1')
+sr.reveal('#prvi', {origin: 'left'})
+sr.reveal('#drugi', {origin: 'right'})
+
+const lightbox = document.createElement('div')
+lightbox.id = 'lightbox'
+document.body.appendChild(lightbox)
+
+
+const emailbtn = document.querySelector('#emailbtn')
+emailbtn.addEventListener('click', e => {
+    lightbox.classList.add('active')
+    const upit = document.querySelector('#upit')
+    upit.style.display = 'block'
+    upit.src = upit.src
+    while (lightbox.firstChild) {
+        lightbox.removeChild(lightbox.firstChild)
+    }
+    lightbox.appendChild(upit)
+})
+
+
+lightbox.addEventListener('click', e => {
+    if (e.target !== e.currentTarget) return
+    lightbox.classList.remove('active')
+})
